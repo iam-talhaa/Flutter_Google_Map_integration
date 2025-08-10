6a0680611cd21a56cf47a6f6e83320f058472e10
unknown <tahakhan4141@gmail.com>
2025-08-10 23:28:28 +0500

    Added Flutter Toast

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -1,3 +1,13 @@
 class Utils {
-  void SHOWTOAST() {}
+  void SHOWTOAST() {
+   Fluttertoast.showToast(
+        msg: "This is Center Short Toast",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    ); 
+  }
 }
