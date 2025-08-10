771ec2260b8496cb5af611fb550a9a1f74cc112b
unknown <tahakhan4141@gmail.com>
2025-08-10 23:29:32 +0500

    Updated

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 class Utils {
-  void SHOWTOAST() {
-   Fluttertoast.showToast(
-        msg: "This is Center Short Toast",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0
-    ); 
+  void SHOWTOAST(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
   }
 }
